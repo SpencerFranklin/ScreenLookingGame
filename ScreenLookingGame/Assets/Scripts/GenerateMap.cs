9ec26e25fa647052411aef7d8c1271df5a44033c
sp3nc3r <franklin.sp@husky.neu.edu>
2017-11-29 13:26:15 -0500

    Fixed error

--- ScreenLookingGame/Assets/Scripts/GenerateMap.cs
+++ ScreenLookingGame/Assets/Scripts/GenerateMap.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GenerateMap : MonoBehaviour {
-	private List<Room> currentMap = new List<GameObject> ();
+	private List<Room> currentMap = new List<Room> ();
 	public List<GameObject> allRooms = new List<GameObject> ();
 	// Use this for initialization
 	void Start () {
