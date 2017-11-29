4e3ce308a745134f1c3060bc3a29b66b41b2fef1
sp3nc3r <franklin.sp@husky.neu.edu>
2017-11-29 13:25:31 -0500

    Added template rooms

---
+++ ScreenLookingGame/Assets/Scripts/GenerateMap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMap : MonoBehaviour {
+	private List<Room> currentMap = new List<GameObject> ();
+	public List<GameObject> allRooms = new List<GameObject> ();
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
