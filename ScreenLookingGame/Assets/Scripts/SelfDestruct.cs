afe90f23f7f592b9c9993129d065423753073cdd
Sawyer Warden <sawyer.warden@gmail.com>
2017-12-06 03:29:54 -0500

    Player Mech

---
+++ ScreenLookingGame/Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour {
+
+	public float delay;
+	float timer = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+
+		if (timer >= delay) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ ScreenLookingGame/Assets/Scripts/Shoot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+	public enum PlayerID{
+		P1,P2,P3,P4
+	}
+	public PlayerID pID;
+	private string playerID;
+
+	public GameObject bullet;
+	public float speed;
+	GameObject barrel;
+
+	// Use this for initialization
+	void Start () {
+		playerID = pID.ToString ();
+		barrel = transform.Find ("Gun").Find("Barrel").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown(playerID + "Fire1")) {
+			GameObject instantiatedProjectile = Instantiate(bullet, barrel.transform.position, barrel.transform.rotation);
+			instantiatedProjectile.GetComponent<Rigidbody>().velocity = barrel.transform.TransformDirection(Vector3.forward*speed);
+		}
+	}
+}
