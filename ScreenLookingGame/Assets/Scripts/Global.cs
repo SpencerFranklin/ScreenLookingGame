163a314b5d675d24d0a8b4ce2c1b2a154beed1ee
sp3nc3r <franklin.sp@husky.neu.edu>
2017-12-06 06:47:13 -0500

    more globals

--- ScreenLookingGame/Assets/Scripts/Global.cs
+++ ScreenLookingGame/Assets/Scripts/Global.cs
@@ -6,6 +6,7 @@ public class Global : MonoBehaviour {
 	public enum Direction {North, South, East, West	};
 	public static List<GameObject> furniture = new List<GameObject>();
 	public static List<GameObject> art = new List<GameObject>();
+	public static List<GameObject> players = new List<GameObject> ();
 
 	Global ins;
 	// Use this for initialization
@@ -19,6 +20,9 @@ public class Global : MonoBehaviour {
 		furniture.Add ((GameObject)Resources.Load ("Table2", typeof(GameObject)));
 		furniture.Add ((GameObject)Resources.Load ("Armchair", typeof(GameObject)));
 		furniture.Add ((GameObject)Resources.Load ("Rocking Chair", typeof(GameObject)));
+
+		players.Add ((GameObject)Resources.Load ("Armchair", typeof(GameObject)));
+		players.Add ((GameObject)Resources.Load ("Rocking Chair", typeof(GameObject)));
 
 		art.Add ((GameObject)Resources.Load ("Painting1", typeof(GameObject)));
 		art.Add ((GameObject)Resources.Load ("Painting2", typeof(GameObject)));
