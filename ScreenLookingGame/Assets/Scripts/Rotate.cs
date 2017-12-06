85557a91cc2f54453fb1ef9f008d6ec8823d71cf
sp3nc3r <franklin.sp@husky.neu.edu>
2017-12-06 05:05:03 -0500

    Extra shit

---
+++ ScreenLookingGame/Assets/Scripts/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+	int[] pick = new int[]{-1, 1};
+	int dir;
+	// Use this for initialization
+	void Start () {
+		dir = pick[Random.Range (0, pick.Length)];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.Rotate (0, dir * 3 * Time.deltaTime, 0);
+	}
+}
